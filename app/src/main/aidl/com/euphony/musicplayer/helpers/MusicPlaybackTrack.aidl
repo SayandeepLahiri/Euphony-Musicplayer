6a4c6d87d7fa7df0fe2eb7a96baa5ffeb5ea4bbf
Sayandeep <sayandeepl63@gmail.com>
2018-03-16 21:23:10 +0530

    Initial commit

---
+++ app/src/main/aidl/com/euphony/musicplayer/helpers/MusicPlaybackTrack.aidl
@@ -0,0 +1,3 @@
+package com.euphony.musicplayer.helpers;
+
+parcelable MusicPlaybackTrack;
---
+++ app/src/main/java/com/euphony/musicplayer/widgets/BubbleTextGetter.java
@@ -0,0 +1,5 @@
+package com.euphony.musicplayer.widgets;
+
+public interface BubbleTextGetter {
+    String getTextToShowInBubble(int pos);
+}
